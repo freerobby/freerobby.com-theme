3eec794b6e9e7579c116fab878de1be15409cf79
Robby Grossman <robby@freerobby.com>
2010-12-30 23:41:13 -0500

    Create page template based on single-post template.

---
+++ vostok/page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+	<div id="content">
+		<div id="main-content">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="post" id="post-<?php the_ID(); ?>">
+				<h2><?php the_title(); ?></h2>
+				<span class="date"><?php the_time(__('l, F j, Y','vostok')) ?></span>
+				<div class="entry">
+					<?php the_content(__('See full page','vostok').' &raquo;'); ?>
+				</div>
+			</div>
+		<?php comments_template(); ?>
+		<?php endwhile; else: ?>
+			<p class="string"><?php _e('That page doesn\'t exist.','vostok'); ?></p>
+	<?php endif; ?>
+		</div><!-- close:main-content -->
+		<?php get_sidebar(); ?>
+	</div><!-- close:content -->
+<?php get_footer(); ?>
