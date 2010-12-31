bb9679c2ede7fd392a5ef8bb74bb49bc6a3fedd1
Robby Grossman <robby@freerobby.com>
2010-12-31 01:27:05 -0500

    Remove "Comments are closed" text; that is implicit if comments are not accepted.

--- vostok/comments.php
+++ vostok/comments.php
@@ -30,13 +30,6 @@
 		<?php endforeach; /* end for each comment */ ?>
 		</ol>
 	</div><!-- close:comments -->
- <?php else : // this is displayed if there are no comments so far ?>
-	<?php if ('open' != $post->comment_status) : ?>
-		<!-- If comments are closed. -->
-	<div id="comments">
-		<h3><?php _e('Comments are closed.','vostok'); ?></h3>
-	</div><!-- close:comments -->
-	<?php endif; ?>
 <?php endif; ?>
 <?php if ('open' == $post->comment_status) : ?>
 	<div id="commentform">
