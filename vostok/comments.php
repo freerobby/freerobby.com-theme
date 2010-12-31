2faf73d6c4c258f50fdf86561e8e8b71d1c900d3
Robby Grossman <robby@freerobby.com>
2010-12-30 23:09:12 -0500

    Minimalist copy regarding comments.

--- vostok/comments.php
+++ vostok/comments.php
@@ -15,7 +15,7 @@
 <!-- You can start editing here. -->
 <?php if ($comments) : ?>
 	<div id="comments">
-		<h3><?php comments_number(__('No comments yet.','vostok'), __('There is 1 comment in this article:','vostok'), __('There are % comments in this article:','vostok') );?></h3>
+		<h3><?php comments_number(__('','vostok'), __('1 comment:','vostok'), __('% comments:','vostok') );?></h3>
 		<ol>
 		<?php foreach ($comments as $comment) : ?>
 			<li id="comment-<?php comment_ID() ?>"  class="comment <?php if (get_comment_author_email() == get_the_author_meta('user_email')) { echo ('highlighted'); }?>">
@@ -31,12 +31,7 @@
 		</ol>
 	</div><!-- close:comments -->
  <?php else : // this is displayed if there are no comments so far ?>
-	<?php if ('open' == $post->comment_status) : ?>
-		<!-- If comments are open, but there are no comments. -->
-	<div id="comments">
-		<h3><?php _e('No comments yet.','vostok'); ?></h3>
-	</div><!-- close:comments -->
-	 <?php else : // comments are closed ?>
+	<?php if ('open' != $post->comment_status) : ?>
 		<!-- If comments are closed. -->
 	<div id="comments">
 		<h3><?php _e('Comments are closed.','vostok'); ?></h3>
